be7e70010ddae75e5fe774db34c90470dfc0e5e8
Natasha Snunkmeox <natmeox@neologasm.org>
2013-03-21 23:10:48 -0400

    Use static files and templates

--- mess/web.go
+++ mess/web.go
@@ -1,19 +1,30 @@
 package mess
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func StartWeb() {
+	staticServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", staticServer))
+
+	indexTemplate, err := template.ParseFiles("./template/index.html", "template/head.html", "template/foot.html")
+	if err != nil {
+		log.Fatalln("Couldn't load HTML templates:", err.Error())
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.String() != "/" {
 			http.NotFound(w, r)
 			return
 		}
 
-		fmt.Fprintf(w, "hi")
+		err := indexTemplate.Execute(w, nil)
+		if err != nil {
+			log.Println("Error executing index.html template:", err.Error())
+		}
 	})
 
 	log.Println("Listening for web requests at address", Config.WebAddress)
